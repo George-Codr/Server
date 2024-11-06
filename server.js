e4375c3427963663384a555f2ededbd40b1f5baf
Mahobin Universe <117584284+Mahobin-Universe@users.noreply.github.com>
2024-11-06 11:53:37 +0600

    Update server.js

--- server.js
+++ server.js
@@ -48,20 +48,27 @@ async function checkSecondaryStatus(res, key) {
 
 // Validate subscription
 async function validateSubscription(res, key) {
-    const subscriptionData = await fetchUrl("https://raw.githubusercontent.com/Mahobin-Universe/Importer/refs/heads/main/SAVAGE/ch3.txt");
+    const blockList = await fetchUrl("https://raw.githubusercontent.com/Mahobin-Universe/Importer/refs/heads/main/SAVAGE/bch.txt");
 
-    // Check if key exists
-    if (subscriptionData.includes(key)) {
-        const [userKey, deviceId, expiryDate, username] = subscriptionData.split("|");
-
-        if (isValidDate(expiryDate)) {
-            const expiry = parseDate(expiryDate);
-            await processSubscription(res, userKey, deviceId, username, expiry);
-        } else {
-            res.status(400).json({ error: "Invalid date format in subscription data." });
-        }
+    // Check if user is blocked
+    if (blockList.includes(key)) {
+        res.status(403).json({ message: "BLOCKED" });
     } else {
-        res.status(401).json({ error: "NONE" });
+        const subscriptionData = await fetchUrl("https://raw.githubusercontent.com/Mahobin-Universe/Importer/refs/heads/main/SAVAGE/ch3.txt");
+
+        // Check if key exists
+        if (subscriptionData.includes(key)) {
+            const [userKey, deviceId, expiryDate, username] = subscriptionData.split("|");
+
+            if (isValidDate(expiryDate)) {
+                const expiry = parseDate(expiryDate);
+                await processSubscription(res, userKey, deviceId, username, expiry);
+            } else {
+                res.status(400).json({ error: "Invalid date format in subscription data." });
+            }
+        } else {
+            res.status(401).json({ error: "NONE" });
+        }
     }
 }
 
